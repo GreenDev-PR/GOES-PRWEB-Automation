898f1f187a7f52b2fdc9faf1c819c5d4af6bd00e
Miguel Garcia Dolagaray <miguel.garcia9@upr.edu>
2014-03-13 15:58:59 -0400

    Implement DIrectoryManager class

--- src/main/com/greendev/pragma/main/DirectoryManager.java
+++ src/main/com/greendev/pragma/main/DirectoryManager.java
@@ -3,15 +3,86 @@ package main.com.greendev.pragma.main;
 import java.io.File;
 import java.util.Date;
 
+/**
+ * Utility class that creates directories needed for program execution
+ * @author miguelgd
+ *
+ */
 public class DirectoryManager {
 
-	public DirectoryManager(String goesDir) {
-		// TODO Auto-generated constructor stub
+	private File rootDirectory;
+	private static String DIRECTORY_FORMAT = "%tY/%tm/%td/";
+
+	public DirectoryManager(String rootDir) {
+		rootDirectory = new File(rootDir);
+		create(rootDirectory);
+		create(getLogDirectory());
 	}
 
+	/**
+	 * Creates root, output and input directory for given date
+	 * @param date
+	 * @return returns the root directory
+	 */
+	public File createDirectoriesForDate(Date date){
+		File dir = getDirectory(date);
+		getInputDirectory(date);
+		getOutputDirectory(date);
+		return dir;	
+	}	
+
+	/**
+	 * Creates output directory for given date
+	 * @param date
+	 * @return
+	 */
+	public File getOutputDirectory(Date date) {
+		File output = new File(getDirectory(date), "/OUTPUT");
+		create(output);
+		return output;
+	}
+
+	/**
+	 * Creates input directory for given date
+	 * @param date
+	 * @return
+	 */
+	public File getInputDirectory(Date date) {
+		File dir = getDirectory(date);
+		File input = new File(dir, "/INPUT");
+		create(input);
+		return input;
+
+	}
+
+	public File getLogDirectory() {
+		return new File(getRootDirectory(), "/LOGS");
+	}
+
+	public File getRootDirectory() {
+		return rootDirectory;
+	}
+
+	/**
+	 * Private utility method used by others to create directories as needed
+	 * @param dir
+	 */
+	private void create(File dir) {
+		if(!dir.exists())
+			dir.mkdirs();	
+	}
+
+	/**
+	 * Creates root directory
+	 * @param date
+	 * @return
+	 */
 	public File getDirectory(Date date) {
-		// TODO Auto-generated method stub
-		return null;
+		String str = DIRECTORY_FORMAT.replaceAll("%t", "%1\\$t");
+		String formatted =  String.format(str, date);
+		File dir = new File(rootDirectory,formatted);
+		create(dir);
+		return dir;
 	}
-
 }
+
