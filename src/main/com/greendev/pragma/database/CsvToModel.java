19a74e0b2a35882ef07caafc0d5449387e2981ca
Miguel Garcia Dolagaray <miguel.garcia9@upr.edu>
2014-03-13 22:00:34 -0400

    Improve CsvToModel and GoesVariable classes, pending testing

--- src/main/com/greendev/pragma/database/CsvToModel.java
+++ src/main/com/greendev/pragma/database/CsvToModel.java
@@ -2,7 +2,10 @@ package main.com.greendev.pragma.database;
 
 import java.io.File;
 import java.io.FileNotFoundException;
-import org.apache.commons.io.FilenameUtils;
+import java.util.ArrayList;
+import java.util.List;
+import java.sql.Date;
+import java.sql.Timestamp;
 
 import csv.impl.CSVReader;
 import main.com.greendev.pragma.database.databaseModel.GoesVariable;
@@ -15,20 +18,18 @@ import main.com.greendev.pragma.database.databaseModel.GoesVariable;
 
 public class CsvToModel {
 
-	private static final int MAXIMUM_ROW_NUMBER = 101;
-	private static final int MAXIMUM_COLUMN_NUMBER = 210;
 	private static CSVReader reader;
 
-	
+
 	/**
 	 * Static method to parse a CSV file into a GoesVariable object
 	 * @param variableName name of the variable being processed (e.g. rainfall, wind, etc.)
 	 * @param csv the File for the .csv with the data
 	 * @return GoesVariable instance loaded with the name of the variable, date, values matrix and map image path
 	 */
-	public static GoesVariable parse(String variableName, File csv){		
+	public static List<GoesVariable> parse(String variableName, File csv, Date date){		
 
-		
+
 		//Read the csv file using CSVReader
 		try {
 			reader = new CSVReader(csv);
@@ -39,46 +40,27 @@ public class CsvToModel {
 
 		//Configuring reader to separate by comma ","
 		reader.setColumnSeparator(',');
-		
-		//Bi-dimensional array to store data values read from csv
-		Double[][] values = new Double[MAXIMUM_ROW_NUMBER][MAXIMUM_COLUMN_NUMBER];
+
+		//List of GoesVariable for each matrix cell, mapped to table rows
+		List<GoesVariable> output = new ArrayList<GoesVariable>();
+
+		Timestamp timeDate = new Timestamp(System.currentTimeMillis());
 		
 		int rowNum = 0;
-		
+
 		//Read by row and then by column to obtain values from the matrix
 		while(reader.hasNext()){
 
 			Object[] row = reader.next();
-			for(int i=0; i < row.length; i++){
-				values[rowNum][i] = Double.parseDouble((String) row[i]);
-				//if(Double.isNaN(values[rowNum][i])) ToDo: decide what to do with NaN 
+			for(int column = 0; column < row.length; column++){			
+				Double value = (Double) Double.parseDouble((String) row[column]);
+				GoesVariable element = new GoesVariable(variableName,rowNum,column,value,timeDate,timeDate);
+				output.add(element);
 			}
-			System.out.println();
 			rowNum++;
 		}
 
-		//GoesVariable object to return
-		GoesVariable out = new GoesVariable();
-		
-		//Setting the name of the variable
-		out.setName(variableName);
-		
-		//Setting the values matrix previously populated
-		out.setValues(values);
-		
-		//Extracting the date from the file name with the following naming convention: variableYYYYMMDD.csv
-		//Setting the date
-		String date = csv.getName().substring(variableName.length());
-		date = date.substring(0, 8);
-		out.setDate(date);
-		
-		//Extracting the map image path from the file name
-		//Located and named same as CSV but with different extension: variableYYYYMMDD.jpg
-		//Setting the map image path
-		String imagePath = FilenameUtils.removeExtension(csv.getAbsolutePath())+".jpg";
-		out.setImagePath(imagePath);
-		
-		return out;
+		return output;
 	}
 
 
--- src/main/com/greendev/pragma/database/databaseModel/GoesVariable.java
+++ src/main/com/greendev/pragma/database/databaseModel/GoesVariable.java
@@ -1,41 +1,72 @@
 package main.com.greendev.pragma.database.databaseModel;
 
+import java.sql.Timestamp;
+
 /**
  * Single GOES-PRWEB variable (e.g. rainfall, ET, wind)
  * Includes the variable name, matrix of values corresponding to PR coordinates,
- * date, and the path to the map image
+ * and date
  * @author miguelgd
  */
 public class GoesVariable {
 
-	private String name;
-	private Double[][] values;
-	String date;
-	private String imagePath;
-		
-	public String getName() {
-		return name;
-	}
-	public void setName(String name) {
-		this.name = name;
-	}
-	public Double[][] getValues() {
-		return values;
-	}
-	public void setValues(Double[][] values2) {
-		this.values = values2;
-	}
-	public String getDate() {
-		return date;
-	}
-	public void setDate(String date) {
-		this.date = date;
-	}
-	public String getImagePath() {
-		return imagePath;
-	}
-	public void setImagePath(String imagePath) {
-		this.imagePath = imagePath;
+	private String variableName;
+	private int row;
+	private int column;
+	private Double value;
+	private Timestamp createdTime;
+	private Timestamp updatedTime;
+	
+	
+	public GoesVariable(String variableName, int row, int column,
+			Double value, Timestamp createdTime, Timestamp updatedTime) {
+		super();
+		this.variableName = variableName;
+		this.row = row;
+		this.column = column;
+		this.value = value;
+		this.createdTime = createdTime;
+		this.updatedTime = updatedTime;
 	}
 	
+	public String getVariableName() {
+		return variableName;
+	}
+	public void setVariableName(String variableName) {
+		this.variableName = variableName;
+	}
+	public Timestamp getCreatedTime() {
+		return createdTime;
+	}
+
+	public void setCreatedTime(Timestamp createdTime) {
+		this.createdTime = createdTime;
+	}
+
+	public Timestamp getUpdatedTime() {
+		return updatedTime;
+	}
+
+	public void setUpdatedTime(Timestamp updatedTime) {
+		this.updatedTime = updatedTime;
+	}
+
+	public int getRow() {
+		return row;
+	}
+	public void setRow(int row) {
+		this.row = row;
+	}
+	public int getColumn() {
+		return column;
+	}
+	public void setColumn(int column) {
+		this.column = column;
+	}
+	public Double getValue() {
+		return value;
+	}
+	public void setValue(Double value) {
+		this.value = value;
+	}
 }
