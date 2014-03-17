eed3ce78da78eba415d7631a7e641a4b91ed1f2b
josediaz30 <jose.diaz30@upr.edu>
2014-03-17 11:15:34 -0400

    Implemented download exists re-attempt mechanism via thread sleep

--- src/main/com/greendev/pragma/download/RetryDownloader.java
+++ src/main/com/greendev/pragma/download/RetryDownloader.java
@@ -2,6 +2,8 @@ package main.com.greendev.pragma.download;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.Timer;
+import java.util.TimerTask;
 
 import org.apache.log4j.Logger;
 
@@ -13,50 +15,60 @@ import org.apache.log4j.Logger;
 public class RetryDownloader implements Downloader {
 
 	private static Logger logger = Logger.getLogger(RetryDownloader.class);
-	private static final int ATTEMPTS = 3;
-	private Downloader dl;
+	//60*60*1000;  
+	private static long WAIT_TIME;
+	private static int ATTEMPTS;
+	private Downloader downloader;
 	
-	public RetryDownloader(Downloader dl){	
-		this.dl = dl;
+	
+	public RetryDownloader(Downloader downloader, int attempts, long waitTime){	
+		this.downloader = downloader;
+		ATTEMPTS = attempts;
+		WAIT_TIME = waitTime;
 	}
-	
+
 	@Override
 	public boolean downloadExists() {
-		return dl.downloadExists();
+		boolean exists = false;
+		int counter = 0;
+
+		logger.debug("Going to check if download exists");
+
+		while(counter < ATTEMPTS){
+			if(this.downloadExists() == false){
+
+				try {
+					Thread.sleep(WAIT_TIME);
+				} catch (InterruptedException e) {
+					logger.debug("Thread sleep was interrupted");
+					e.printStackTrace();
+				}
+				counter++;	
+			}else{
+				return exists = this.downloadExists();
+			}
+		}
+
+		return exists;
 	}
+	
+	
+	
 
 	@Override
 	public File download() throws IOException {
-		//attemp to download
-		
-		Download result;
-		
-		int counter = 0;
-		
-		while(counter < ATTEMPTS){
-			
-			try{
-				return dl.download();
-			}catch(IOException e){
-				//robustness
-				counter++;
-			}
-
-		}
-		
-		//result.getUrl();
-		//File yesterday = new File(path, 
-		return null;
+		//attempt to download
+		return downloader.download();
 	}
 
 	@Override
 	public void setDownload(Download download) {
-		dl.setDownload(download);
+		downloader.setDownload(download);
 	}
 
 	@Override
 	public Download getDownload() {
-		return dl.getDownload();
+		return downloader.getDownload();
 	}
 
 }
