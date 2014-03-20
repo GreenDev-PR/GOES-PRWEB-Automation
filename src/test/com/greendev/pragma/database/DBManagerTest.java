6e4586ea2e1b4495d8a19b6180417a8a8fffb68a
josediaz30 <jose.diaz30@upr.edu>
2014-03-20 00:10:49 -0400

    Completed data insertion integrity test.
    Added GoesVariable Bean container class, in order to wrap around SQL to Joda's DateTime incompatibility issue.

--- src/test/com/greendev/pragma/database/DBManagerTest.java
+++ src/test/com/greendev/pragma/database/DBManagerTest.java
@@ -46,53 +46,46 @@ public class DbManagerTest {
 
 	private static final String DELETE_GOES_DATA_RECORDS_QUERY = "DELETE FROM GoesData"; 
 	private static final String DELETE_GOES_MAPS_RECORDS_QUERY = "DELETE FROM GoesMaps"; 
-	
+
 	private static final String READ_GOES_DATA = "SELECT * FROM GoesData";
 	private static final String READ_GOES_MAPS = "SELECT * FROM GoesMaps";
-	
+
 	private static  Map<String,String> map;
 	private static  BeanProcessor processor;
-	
+
 	private static final String USERNAME = "postgres";
 	private static final String PASSWORD = null;
-	
+
 	private static Connection conn;
 	private static DbManager dbManager;
-	
+
 	private static String varName;
 	private static File csvFile;
 	private static DateTime date;
-	
+
 	private static List<String> varNameList;
 	private static File mapDir;
-	
+
 	@BeforeClass
 	public static void setUpBeforeClass() throws Exception {
 		System.out.println("Executing @BeforeClass - setUpBeforeClass()");
-		map = new HashMap<String,String>();
-		map.put("variablename", "variableName");
-		map.put("matrixrow", "row");
-		map.put("matrixcolumn", "column");
-		map.put("datavalue", "dataValue");
-		map.put("datadate",  "(DateTime) dataDate");
-		//map.put("createdat", "createdAt");
-		//map.put("updatedat", "updatedAt");
-	
-		
-		processor = new BeanProcessor(map);
+
 		//initialize common test dependencies
-		conn = createConnection();
+		conn = DriverManager.getConnection("jdbc:postgresql://localhost:5432/pragma_test",USERNAME,PASSWORD);
 		System.out.println("Database connection succesfully established");
 		dbManager = new DbManager(conn);
 		System.out.println("Created dbManager instance");
 
+		//initialize GoesDataIntegrityTest dependencies
+		initializeGoesDataIntegrityDependencies();
+
 		//initialize GoesDataTest dependencies
 		initializeGoesDataTestDependancies();
-		
+
 		//initialize GoesMapsTest dependencies
 		initializeGoesMapsTestDependencies();
 		System.out.println("SetupBeforeClass successfull");
-		
+
 		//Clean up before testing
 		QueryRunner run = new QueryRunner();
 		run.update(conn, DELETE_GOES_DATA_RECORDS_QUERY);
@@ -100,17 +93,6 @@ public class DbManagerTest {
 	}
 
 	/****** Initialization methods ********/
-	
-	/**
-	 * Create a new db connection 
-	 * @return
-	 * @throws SQLException
-	 */
-	private static Connection createConnection() throws SQLException{
-		System.out.println("Executing @HelperMethod - createConnection()");
-		return DriverManager.getConnection("jdbc:postgresql://localhost:5432/pragma_test",USERNAME,PASSWORD);
-	}
-	
 	private static void initializeGoesDataTestDependancies(){
 		//assign variable name: MUST BE A VALID FOREIGN KEY
 		varName = "Bowen_Ratio";
@@ -130,23 +112,30 @@ public class DbManagerTest {
 		//Test directory, current directory + /OUTPUT/
 		mapDir = new File("src/test/com/greendev/pragma/database/OUTPUT/");
 	}
-	
+
+	private static void initializeGoesDataIntegrityDependencies(){
+		//Setup Bean Processor
+		map = new HashMap<String,String>();
+		map.put("variablename", "variableName");
+		map.put("matrixrow", "row");
+		map.put("matrixcolumn", "column");
+		map.put("datavalue", "dataValue");
+		map.put("datadate",  "dataDate");
+		processor = new BeanProcessor(map);
+	}
+
 	/************* Testing methods **************/
-	
-	
 	//@Test 
 	public void readGoesDataTest() throws SQLException{
 		System.out.println("Executing @Test - readGoesVariableTest()");
-		
-		
+
 		List<String> resultList = dbManager.readGoesVariables();
 		assertNotNull(resultList);
 		for(String str: resultList)
 			assertTrue(resultList.contains(str));
-		
 	}
-	
-	//@Test
+
+	@Test
 	public void storeGoesDataTest() throws SQLException, FileNotFoundException {
 		System.out.println("Executing @Test - storeGoesDataTest()");
 		//Parse csv data to Goes Variable object
@@ -158,55 +147,52 @@ public class DbManagerTest {
 			if(!Double.isNaN(varList.get(i).getDataValue()))
 				numericValuesInCsv++;
 		}
-		
+
 		System.out.println("Executing dbManager.storeGoesData method ");
 		int dbInserts = dbManager.storeGoesData(varName, csvFile, date);
-	
+
 		System.out.println("Expected inserts: "+numericValuesInCsv);
 		System.out.println("Actual inserts: "+dbInserts);
 		//It is expected that the number of numericValues in Csv 
 		//should be equal to the number of inserts perfomred
 		assertTrue(dbInserts == numericValuesInCsv);
-		
+
 	}
-	
-	//@Test
+
+	@Test
 	public void storeGoesMapsTest() throws SQLException{
 		System.out.println("Executing @Test - storeGoesMapsTest()");
 		int inserts = dbManager.storeGoesMap(varNameList, mapDir, date);
 		
 		assertTrue(inserts == varNameList.size());
 	}
-	
+
 	@Test
 	public void verifyDataInserted() throws FileNotFoundException, SQLException{
-		
-		Object[] beanArray;
-		QueryRunner run = new QueryRunner();
-		
+
 		//Resultset Handler
 		ResultSetHandler<Object[]> h = new ResultSetHandler<Object[]>(){
 			Object[] result = new Object[dbManager.storeGoesData(varName, csvFile, date)];
-			
+
 			public Object[] handle(ResultSet rs) throws SQLException {
-		    	
+
 				int i = 0;
-		    	GoesVariable row = null;
-		    	//Create JavaBeans of each GoesVariable
-		    	while(rs.next()){
-		    		row = processor.toBean(rs, GoesVariable.class);
-		    		System.out.println("Bean: "+row);
-		    		result[i] = row;
-		        	i++;
-		    	}   
-		    	return result;
-		    }
+				GoesVariableBean row = null;
+				//Create JavaBeans of each GoesVariable
+				while(rs.next()){
+					row = processor.toBean(rs, GoesVariableBean.class);
+					result[i] = row;
+					i++;
+				}   
+				return result;
+			}
 		};
-		
+
+		QueryRunner run = new QueryRunner();
 		//Run READ_GOES_DATA query
 		//The data is read to bean array
-		beanArray = run.query(conn, READ_GOES_DATA, h);
-	
+		Object[] beanArray = run.query(conn, READ_GOES_DATA, h);
+
 		/****************************************************************************
 		 * Simulate the insertion method with data extracted directly from CSV file
 		 ****************************************************************************/
@@ -215,30 +201,27 @@ public class DbManagerTest {
 		//inserted if the dataValue is NOT a NaN value
 		//therefore we simulate the same effect as in the
 		//method by filtering the data to a list.
-		
+
 		//Extract all values from CSV 
 		List<GoesVariable> dataInCsvList = CsvToModel.parse(varName, csvFile, date);
-		List<GoesVariable> NaNFilteredCSVData = new ArrayList<GoesVariable>();
-		
+		List<GoesVariableBean> NaNFilteredCSVData = new ArrayList<GoesVariableBean>();
+
 		//Filter NaN values from CSV data
 		for(GoesVariable variable : dataInCsvList){
 			if(!Double.isNaN(variable.getDataValue()))
-			NaNFilteredCSVData.add(variable);
+				NaNFilteredCSVData.add(new GoesVariableBean(variable));
 		}
 		/***************************************************************************
 		 * Query result Comparison Vs. Filtered CSV data
 		 ***************************************************************************/
-		
+
 		//Compare for member filtered CSV data to query result data.
+		//This verifies data integrity
 		for(int i=0; i<NaNFilteredCSVData.size(); i++){
+		//	System.out.println("Bean: "+beanArray[i]);
+		//	System.out.println("CSV: "+NaNFilteredCSVData.get(i));
 			assertTrue(NaNFilteredCSVData.contains(beanArray[i]));
 		}
-		/*Compare for data integritiy
-		for(int i=0; i<NaNFilteredCSVData.size(); i++){
-			assertTrue(NaNFilteredCSVData.get(i).getVariableName().equals(beanArray[i])); 
-			
-		}
-		End of test*/
 	}
 
 	@Before
@@ -253,7 +236,7 @@ public class DbManagerTest {
 		run.update(conn, DELETE_GOES_DATA_RECORDS_QUERY);
 		run.update(conn, DELETE_GOES_MAPS_RECORDS_QUERY);
 	}
-	
+
 	@AfterClass
 	public static void tearDownAfterClass() throws Exception {
 		System.out.println("Executing @AfterClass - tearDownAfterClass()");
@@ -264,7 +247,7 @@ public class DbManagerTest {
 		DbUtils.close(conn); //close db connection
 		conn = null;
 		System.out.println("Database connection for GoesMaps closed on: "+new Timestamp(System.currentTimeMillis()));
-		
+
 	}
 
 
